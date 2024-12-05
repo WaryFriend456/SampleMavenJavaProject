f32ca87733949d3ae6af02593939247d0abeca5e
Kushal Bang <134197372+WaryFriend456@users.noreply.github.com>
2024-12-05 20:07:36 +0530

    Update App.java

--- src/main/java/com/app/test/SampleMavenJavaProject/App.java
+++ src/main/java/com/app/test/SampleMavenJavaProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! testing for POLL SCM" );
+        System.out.println( "Hello World! testing for POLL SCM automation" );
     }
 }
