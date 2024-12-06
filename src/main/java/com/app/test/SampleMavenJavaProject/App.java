55a73cbf3cb22612e51f29946aa6adcb018782d0
Kushal Bang <134197372+WaryFriend456@users.noreply.github.com>
2024-12-06 10:13:28 +0530

    Update App.java POLL SCM Test

--- src/main/java/com/app/test/SampleMavenJavaProject/App.java
+++ src/main/java/com/app/test/SampleMavenJavaProject/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! testing for POLLSCM" );
+        System.out.println( "Hello World! testing for POLLSCM changes" );
+        System.out.println( "Hello World! testing for POLLSCM changes" );
     }
 }
